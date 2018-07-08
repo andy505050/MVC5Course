99b93b4eb4224b01e8521e14b1169e8329393879
Tom <alex60510@hotmail.com>
2018-07-08 15:53:29 +0800

    routing

--- MVC5Course/Controllers/ClientsController.cs
+++ MVC5Course/Controllers/ClientsController.cs
@@ -10,6 +10,7 @@ using MVC5Course.Models;
 
 namespace MVC5Course.Controllers
 {
+    [RoutePrefix("xxx")]
     public class ClientsController : Controller
     {
         //private FabricsEntities db = new FabricsEntities();
@@ -22,19 +23,20 @@ namespace MVC5Course.Controllers
             occup = RepositoryHelper.GetOccupationRepository(repo.UnitOfWork);
         }
         // GET: Clients
+        [Route("Index")]
         public ActionResult Index()
         {
             var client = repo.All();
             return View(client.Take(10).OrderByDescending(x => x.ClientId).ToList());
         }
-
+        [Route("S")]
         public ActionResult Search(string Keyword)
         {
             var data = repo.Search(Keyword);
 
             return View("Index", data.Take(10));
         }
-
+        [Route("D/{id}")]
         // GET: Clients/Details/5
         public ActionResult Details(int? id)
         {
@@ -49,7 +51,7 @@ namespace MVC5Course.Controllers
             }
             return View(client);
         }
-
+        [Route("C")]
         // GET: Clients/Create
         public ActionResult Create()
         {
@@ -63,6 +65,7 @@ namespace MVC5Course.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Route("C")]
         public ActionResult Create([Bind(Include = "ClientId,FirstName,MiddleName,LastName,Gender,DateOfBirth,CreditRating,XCode,OccupationId,TelephoneNumber,Street1,Street2,City,ZipCode,Longitude,Latitude,Notes,IDnumber")] Client client)
         {
             if (ModelState.IsValid)
@@ -77,6 +80,7 @@ namespace MVC5Course.Controllers
         }
 
         // GET: Clients/Edit/5
+        [Route("E/{id}")]
         public ActionResult Edit(int? id)
         {
             if (id == null)
@@ -97,6 +101,7 @@ namespace MVC5Course.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Route("E/{id}")]
         public ActionResult Edit([Bind(Include = "ClientId,FirstName,MiddleName,LastName,Gender,DateOfBirth,CreditRating,XCode,OccupationId,TelephoneNumber,Street1,Street2,City,ZipCode,Longitude,Latitude,Notes,IDnumber")] Client client)
         {
             if (ModelState.IsValid)
@@ -111,6 +116,7 @@ namespace MVC5Course.Controllers
         }
 
         // GET: Clients/Delete/5
+        [Route("DD/{id}")]
         public ActionResult Delete(int? id)
         {
             if (id == null)
@@ -126,8 +132,9 @@ namespace MVC5Course.Controllers
         }
 
         // POST: Clients/Delete/5
-        [HttpPost, ActionName("Delete")]
+        [HttpPost, ActionName("DD")]
         [ValidateAntiForgeryToken]
+        [Route("DD/{id}")]
         public ActionResult DeleteConfirmed(int id)
         {
             Client client = repo.Find(id);
